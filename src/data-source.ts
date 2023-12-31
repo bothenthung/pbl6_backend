f13f7ad5de82fa2b853ce84e2f3298bba2a93fd8
Xơn Phạm Hoàng <xonhoang@Xons-MacBook-Air.local>
2023-12-31 20:41:19 +0700

    fix: resolve conflict

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,13 @@
 import { DataSource } from "typeorm";
+import serverConfig from "./core/config";
 import { ColumnEntity } from "./entities/Column.entity";
 import { InvitationEntity } from "./entities/Invitation.entity";
 import { MessageEntity } from "./entities/Message.entity";
+import { NotificationEntity } from "./entities/Notification.entity";
 import { ProjectEntity } from "./entities/Project.entity";
+import { ProjectUserEntity } from "./entities/ProjectUser.entity";
 import { TaskEntity } from "./entities/Task.entity";
 import { UserEntity } from "./entities/User.entity";
-import { NotificationEntity } from "./entities/Notification.entity";
-import { ProjectUserEntity } from "./entities/ProjectUser.entity";
-import serverConfig from "./core/config";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
